53bda2aaf9f7a54069b7463341328a824a078161
Mason More <masonmore@Briannas-MacBook-Pro.local>
2020-09-16 14:55:35 -0400

    initial xcode project creation

---
+++ youtube-one-day-build/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-one-day-build
+//
+//  Created by Mason More on 2020-09-16.
+//  Copyright © 2020 Mason More. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
